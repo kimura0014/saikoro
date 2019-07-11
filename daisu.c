a4724f42af7ed8ee87a01f943d6cb9e0ee831eee
s1260091 <s1260091@std5dc26.u-aizu.ac.jp>
2019-07-11 18:51:53 +0900

    add aisatsu

--- daisu.c
+++ daisu.c
@@ -6,13 +6,18 @@ int main(){
   srand(time(NULL));
   int a=rand()%6+1;
   int b=rand()%6+1;
+  char name[70];
+
+  
+  printf("What is your name?\n>");
+  scanf("%s",name);
+  printf("Hello,%s!\n",name);
+
   printf("Rolling the dice...\n");
   printf("Die 1:%d\n",a);
   printf("Die 2:%d\n",b);
   int total=a+b;
   printf("Total value:%d\n",total);
-  
-  
 
 
   return 0;
