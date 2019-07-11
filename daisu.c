3f3823d140b34def53704cc987d4fd5d319a0a4b
s1260091 <s1260091@std5dc26.u-aizu.ac.jp>
2019-07-11 18:43:26 +0900

    first revision

---
+++ daisu.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+int main(){
+  srand(time(NULL));
+  int a=rand()%6+1;
+  int b=rand()%6+1;
+  printf("Rolling the dice...\n");
+  printf("Die 1:%d\n",a);
+  printf("Die 2:%d\n",b);
+  int total=a+b;
+  printf("Total value:%d\n",total);
+  
+  
+
+
+  return 0;
+}
